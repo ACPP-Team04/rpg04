443b45c70926baab773ce5bc726e7dd66d9e6fba
Marco <marco.cotrotzo@student.uibk.ac.at>
2026-03-17 14:10:01 +0100

    addPipeline

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marco/rpg04
-# Build directory: /home/marco/rpg04/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "/home/marco/rpg04/build/tests")
-set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "/home/marco/rpg04/CMakeLists.txt;32;add_test;/home/marco/rpg04/CMakeLists.txt;0;")
