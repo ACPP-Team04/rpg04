3291818b43c80ab3fcb9b44e184a08ec27de5d5b
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-03-29 19:46:42 +0200

    Add initial test for archetypeManager

---
+++ src/test/Abstract/ECS/Archetype/ArchetypeManagerTest.cpp
@@ -0,0 +1,7 @@
+#include "Abstract/ECS/Archetype/ArchetypeManager.hpp"
+#include <gtest/gtest.h>
+
+TEST(ArchetypeManager, createEntity)
+{
+	ArchetypeManager archetypeManager = ArchetypeManager();
+}
