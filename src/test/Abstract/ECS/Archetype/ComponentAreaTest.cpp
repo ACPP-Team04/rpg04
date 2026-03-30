c198ea056014a4fbb85d90cb3761134dd1c8246c
csba1184 <107847988+mcotrotzo@users.noreply.github.com>
2026-03-30 03:53:03 +0200

    createUnitTestForArcheTypeManager

--- src/test/Abstract/ECS/Archetype/ComponentAreaTest.cpp
+++ src/test/Abstract/ECS/Archetype/ComponentAreaTest.cpp
@@ -18,6 +18,7 @@ class MockPool : public IPool {
 	size_t lastCopyNewIndex = 0;
 	IPool *lastCopyTargetPool = nullptr;
 	std::unique_ptr<IPool> createEmpty() { return std::make_unique<MockPool>(); }
+
 	size_t currentSize = 0;
 
 	size_t addEntity() override
