c0ed9b1208de613b06a500c1afb23f1a1c4d4255
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-04-01 18:22:22 +0200

    Add tgui to ECSManager

--- src/main/Abstract/ECS/ECSManager.hpp
+++ src/main/Abstract/ECS/ECSManager.hpp
@@ -2,30 +2,25 @@
 #include "Archetype/ArchetypeManager.hpp"
 #include "System/System.hpp"
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
+#include <TGUI/TGUI.hpp>
 
 struct ECSManager {
 
-	sf::RenderWindow& window;
+	sf::RenderWindow &window;
 	ArchetypeManager manager = ArchetypeManager();
+	tgui::Gui gui;
 
-	ECSManager(sf::RenderWindow& window) : window(window) {}
+	ECSManager(sf::RenderWindow &window) : window(window), gui(window) {}
 
 	~ECSManager() = default;
 	void processEvents()
 	{
-		while (const std::optional event = window.pollEvent())
-		{
+		while (const std::optional event = window.pollEvent()) {
+			gui.handleEvent(*event);
 			if (event->is<sf::Event::Closed>())
 				window.close();
 		}
-
-
 	}
-
-	void update()
-	{
-		processEvents();
-	}
-
-
+	void update() { processEvents(); }
 };
