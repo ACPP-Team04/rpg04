3750a2456fa9fccf71ae46ec6a105246b52cdf5e
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-04-02 21:26:53 +0200

    AISystem for enemies

---
+++ src/main/Implementation/Systems/AISystem.cpp
@@ -0,0 +1,38 @@
+#include "Abstract/Combat/Systems/AISystem.hpp"
+#include "Abstract/Combat/Systems/CombatSystem.hpp"
+#include "Abstract/ECS/Archetype/ArchetypeManager.hpp"
+#include "Abstract/ECS/Entity/EntityID.hpp"
+#include "Abstract/ECS/System/System.hpp"
+#include "Implementation/Components/BattleComponent.hpp"
+#include "Implementation/Components/InventoryComponent.hpp"
+#include "Implementation/Components/StatsComponent.hpp"
+
+AISystem::AISystem(ArchetypeManager &manager) : System(manager) {};
+void AISystem::executeAILogic(EntityID aiId, std::vector<EntityID> participants)
+{
+	BattleComponent &aiBattle = manager.getComponent<BattleComponent>(aiId);
+	StatsComponent &aiStats = manager.getComponent<StatsComponent>(aiId);
+	InventoryComponent &aiInventory = manager.getComponent<InventoryComponent>(aiId);
+	EntityID target;
+	for (EntityID p : participants) {
+		if (!(aiId != p)) {
+			target = p;
+			break;
+		}
+	}
+	aiBattle.target = target;
+
+	if (aiStats.health < 30 && aiInventory.numberOfHealthPotions >= 1) {
+		aiBattle.selectedAction = BattleAction::HEAL;
+	} else if (aiBattle.AP >= CombatSystem::getActionCost(BattleAction::HEAVY_ATTACK)) {
+		aiBattle.selectedAction = BattleAction::HEAVY_ATTACK;
+	} else if (aiBattle.AP >= CombatSystem::getActionCost(BattleAction::LIGHT_ATTACK)) {
+		aiBattle.selectedAction = BattleAction::LIGHT_ATTACK;
+	} else if (aiBattle.numberOfUltimateAttacksUsed < 1) {
+		aiBattle.selectedAction = BattleAction::ULTIMATE_ATTACK;
+	} else {
+		aiBattle.selectedAction = BattleAction::REST;
+	}
+	aiBattle.battleState = BattleState::SELECTED_ACTION;
+}
+void AISystem::update() {};
