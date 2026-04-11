994bbca270bb29b69ea32585c8395d5ce51014fb
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-04-11 12:29:41 +0200

    Logic for automatic euqipment

--- src/main/Abstract/Overwordl/Components/InventoryComponent.hpp
+++ src/main/Abstract/Overwordl/Components/InventoryComponent.hpp
@@ -7,7 +7,7 @@ struct InventoryComponent : public Component<InventoryComponent> {
 	std::vector<EntityID> inventory;
 	std::unordered_map<ITEM_TYPE, std::unordered_set<EntityID>> items;
 
-	std::unordered_map<ITEM_TYPE,EntityID> equiped;
+	std::unordered_map<ITEM_TYPE, EntityID> equiped;
 
 	void readFromJson(const nlohmann::json &j) override {}
 
@@ -18,7 +18,9 @@ struct InventoryComponent : public Component<InventoryComponent> {
 		}
 
 		items[item].insert(entity);
-		equiped[item] = entity;
+		if (!equiped.contains(item)) {
+			equiped[item] = entity;
+		}
 	}
 
 	void removeItem(EntityID entity, ITEM_TYPE item)
@@ -26,7 +28,7 @@ struct InventoryComponent : public Component<InventoryComponent> {
 		if (items.contains(item)) {
 			items[item].erase(entity);
 
-			if (getEquippedItem(item)==entity) {
+			if (getEquippedItem(item) == entity) {
 				equiped.erase(item);
 			}
 		}
@@ -61,16 +63,13 @@ struct InventoryComponent : public Component<InventoryComponent> {
 		return false;
 	}
 
-
-	void equip(EntityID itemId,ITEM_TYPE item)
+	void equip(EntityID itemId, ITEM_TYPE item)
 	{
 		if (containsItem(itemId)) {
 			this->equiped[item] = itemId;
-		}
-		else {
+		} else {
 			throw std::runtime_error("Inventory does not have this item to equip");
 		}
-
 	}
 
 	EntityID getEquippedItem(ITEM_TYPE item)
@@ -79,11 +78,20 @@ struct InventoryComponent : public Component<InventoryComponent> {
 			if (equiped.contains(item)) {
 				return equiped[item];
 			}
-		}
-		else {
+		} else {
 			throw std::runtime_error("No equipment");
 		}
-
 	}
 
+	bool hasEquippedItem(ITEM_TYPE item)
+	{
+		if (items.contains(item)) {
+			if (equiped.contains(item)) {
+				return true;
+			}
+		} else {
+			return false;
+		}
+		return false;
+	}
 };
