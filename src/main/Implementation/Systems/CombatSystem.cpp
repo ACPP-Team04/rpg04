066819bef50c87d214f50d4fc49d38e1df4045a3
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-04-16 20:54:14 +0200

    Add logs to combatSystem

--- src/main/Implementation/Systems/CombatSystem.cpp
+++ src/main/Implementation/Systems/CombatSystem.cpp
@@ -78,7 +78,7 @@ void CombatSystem::update()
 				this->passTurn(currentAttacker, bmc.currentTurnIndex, bmc.participants);
 				break;
 			case BattleState::VICTORY:
-				spdlog::get("combat")->debug("Victory for player");
+				spdlog::get("combat")->info("Victory for player");
 				bmc.isBattleOver = true;
 				break;
 			case BattleState::DEFEAT:
@@ -160,12 +160,12 @@ void CombatSystem::executeBattleAction(EntityID attacker, EntityID defender, Bat
 	}
 	spdlog::get("combat")->info("Entity {} attacks Entity {} with {}!", attacker.getId(), defender.getId(),
 	                            static_cast<int>(typeOfAction));
-	spdlog::get("combat")->debug("Entity {} has {} HP and {} AP left!", defender.getId(),
-	                             manager.getComponent<StatsComponent>(defender).health,
-	                             manager.getComponent<BattleComponent>(defender).AP);
-	spdlog::get("combat")->debug("Entity {} has {} HP and {} AP left!", attacker.getId(),
-	                             manager.getComponent<StatsComponent>(attacker).health,
-	                             manager.getComponent<BattleComponent>(attacker).AP);
+	spdlog::get("combat")->info("Entity {} has {} HP and {} AP left!", defender.getId(),
+	                            manager.getComponent<StatsComponent>(defender).health,
+	                            manager.getComponent<BattleComponent>(defender).AP);
+	spdlog::get("combat")->info("Entity {} has {} HP and {} AP left!", attacker.getId(),
+	                            manager.getComponent<StatsComponent>(attacker).health,
+	                            manager.getComponent<BattleComponent>(attacker).AP);
 }
 
 void CombatSystem::takeHealAction(EntityID healer, int faith, int maxHealth)
@@ -241,6 +241,11 @@ void CombatSystem::cleanUpBattle(EntityID battleManagerId, EntityID winningEntit
 	for (EntityID entity : participantsCopy) {
 		manager.removeComponentFromEntity<BattleComponent>(entity);
 		auto &stats = manager.getComponent<StatsComponent>(entity);
+		if (stats.health > stats.getStat(STATS::MAX_HEALTH)) {
+			spdlog::get("combat")->warn(
+			    "HP of Entity {} have been set to max_health {}, which is lower than current health {}", entity.getId(),
+			    stats.getStat(STATS::MAX_HEALTH), stats.health);
+		}
 		stats.health = stats.getStat(STATS::MAX_HEALTH);
 		if (entity == winningEntity && battleState == BattleState::VICTORY) {
 			stats.experienceLevel += 1;
