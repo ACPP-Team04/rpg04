25393a2bfaf8e24d712cba7c845e97acde3f33fd
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-04-18 18:41:28 +0200

    Start implementing the logic for more enemies in the fight

--- src/main/Abstract/Overwordl/SwitchBattleModeSystem.hpp
+++ src/main/Abstract/Overwordl/SwitchBattleModeSystem.hpp
@@ -2,9 +2,18 @@
 
 #pragma once
 #include "Abstract/ECS/System/System.hpp"
+#include <SFML/Graphics/RenderWindow.hpp>
 
 struct SwitchBattleModeSystem : System {
 
 	SwitchBattleModeSystem(ArchetypeManager &manager);
 	void update() override;
+	[[nodiscard]] std::vector<EntityID> getEnemiesInRatio(const sf::Vector2f center, float radius);
+	void prepareForBattle(const std::vector<EntityID> &participants, EntityID playerId);
+	static float getSquaredDistance(const sf::Vector2f &a, const sf::Vector2f &b)
+	{
+		float dx = a.x - b.x;
+		float dy = a.y - b.y;
+		return (dx * dx) + (dy * dy);
+	}
 };
