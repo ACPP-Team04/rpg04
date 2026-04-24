0e84eb5079f5447a43bb37b254a794048d0fdb33
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-04-24 20:43:07 +0200

    Add grave display, when enemy dies

--- src/main/Implementation/Overworld/RenderSystem.cpp
+++ src/main/Implementation/Overworld/RenderSystem.cpp
@@ -7,14 +7,15 @@
 #include "Abstract/Overwordl/Components/TransformComponent.hpp"
 #include "Abstract/Overwordl/Components/WorldComponent.hpp"
 #include "Abstract/Utils/WorldUtlis.hpp"
+#include <Abstract/Combat/Components/DeathComponent.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 
 RenderSystem::RenderSystem(ArchetypeManager &manager, sf::RenderWindow &window) : System(manager), window(window) {};
 
-void render(TransformComponent &tcomp, sf::RenderWindow &window,
-            SpriteComponent &scomp)
+void render(TransformComponent &tcomp, sf::RenderWindow &window, SpriteComponent &scomp)
 {
 
-	sf::Sprite sp = AssetManager::getInstance().getSpriteAt(scomp);	
+	sf::Sprite sp = AssetManager::getInstance().getSpriteAt(scomp);
 	sp.setPosition(tcomp.position);
 	sp.setScale(tcomp.scale);
 	sp.setRotation(tcomp.rotation);
@@ -22,17 +23,38 @@ void render(TransformComponent &tcomp, sf::RenderWindow &window,
 }
 void RenderSystem::update()
 {
-	WorldUtils::viewInCurrentLayer<RenderComponent, TransformComponent, SpriteComponent>(manager,
-		[&](const EntityID &id, RenderComponent &comp, TransformComponent &tcomp, SpriteComponent &scomp) {
-			if (comp.z_layer == 0) {
-				render(tcomp, window, scomp);
-			}
-		});
+	auto renderLogic = [&](const EntityID &id, RenderComponent &comp, TransformComponent &tcomp,
+	                       SpriteComponent &scomp) {
+		if (manager.hasComponent<DeathComponent>(id)) {
+			/*
+			// DEBUG
+			sf::RectangleShape debugBox;
+			debugBox.setSize({(float)scomp.tileInfo.width, (float)scomp.tileInfo.height});
+			debugBox.setPosition(tcomp.position);
+			debugBox.setFillColor(sf::Color::Red);
+			window.draw(debugBox);
+			*/
+			auto &deathC = manager.getComponent<DeathComponent>(id);
+			SpriteComponent graveVisuals;
+			graveVisuals.tileInfo = deathC.graveTile;
+			graveVisuals.tilesetPath = deathC.graveTilesetPath;
+			render(tcomp, window, graveVisuals);
+		} else {
+			render(tcomp, window, scomp);
+		}
+	};
 
-	WorldUtils::viewInCurrentLayer<RenderComponent, TransformComponent, SpriteComponent>(manager,
-		[&](const EntityID &id, RenderComponent &comp, TransformComponent &tcomp, SpriteComponent &scomp) {
-			if (comp.z_layer) {
-				render(tcomp, window, scomp);
-			}
-		});
+	WorldUtils::viewInCurrentLayer<RenderComponent, TransformComponent, SpriteComponent>(
+	    manager, [&](const EntityID &id, RenderComponent &comp, TransformComponent &tcomp, SpriteComponent &scomp) {
+		    if (comp.z_layer == 0) {
+			    renderLogic(id, comp, tcomp, scomp);
+		    }
+	    });
+
+	WorldUtils::viewInCurrentLayer<RenderComponent, TransformComponent, SpriteComponent>(
+	    manager, [&](const EntityID &id, RenderComponent &comp, TransformComponent &tcomp, SpriteComponent &scomp) {
+		    if (comp.z_layer) {
+			    renderLogic(id, comp, tcomp, scomp);
+		    }
+	    });
 }
