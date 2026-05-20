7554c97628396c9c8958571d2363c2f7ce2e4285
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-05-20 12:04:47 +0200

    Create GameState class

---
+++ src/main/Abstract/GameState/GameState.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <map>
+#include <set>
+#include <string>
+
+class GameState {
+  public:
+	static GameState &getInstance()
+	{
+		static GameState instance;
+		return instance;
+	}
+
+	std::set<std::string> deadUniqueEntities;
+
+	std::map<std::string, int> entityStates;
+
+	void clearAll()
+	{
+		deadUniqueEntities.clear();
+		entityStates.clear();
+	}
+
+	GameState(const GameState &) = delete;
+	GameState &operator=(const GameState &) = delete;
+
+  private:
+	GameState() = default;
+};
