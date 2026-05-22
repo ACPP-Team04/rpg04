609061c3ca6202f2c2df5417189d7ffd30d4d45e
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-05-22 22:05:45 +0200

    Delete componts outside of the view

--- src/main/Implementation/Overworld/ItemSystem.cpp
+++ src/main/Implementation/Overworld/ItemSystem.cpp
@@ -1,4 +1,3 @@
-
 #include "Abstract/Overwordl/ItemSystem.hpp"
 
 #include "Abstract/Overwordl/Components/CharacterComponent.hpp"
@@ -12,32 +11,32 @@
 #include "Abstract/Overwordl/WorldParser.hpp"
 #include "Abstract/Utils/WorldUtlis.hpp"
 
-ItemSystem::ItemSystem(ArchetypeManager &manager) : System(manager) {}
+ItemSystem::ItemSystem(ArchetypeManager& manager) : System(manager) {}
 
 void ItemSystem::update()
 {
-	WorldComponent *world = WorldUtils::getWorld(manager);
+	WorldComponent* world = WorldUtils::getWorld(manager);
 	EntityID player = WorldUtils::getPlayer(manager).value();
 
 	if (!manager.hasComponent<InputComponent>(player)) {
 		return;
 	}
-	CharacterComponent &character_component = manager.getComponent<CharacterComponent>(player);
+	CharacterComponent& character_component = manager.getComponent<CharacterComponent>(player);
 	std::cout << "CharacterCatch" << std::endl;
-	InputComponent &inputComponent = manager.getComponent<InputComponent>(player);
+	InputComponent& inputComponent = manager.getComponent<InputComponent>(player);
 	std::cout << character_component.inventory.items.size() << std::endl;
 	WorldUtils::viewInCurrentLayer<InteractionComponent, ItemComponent>(
-	    manager, [&](auto &entity, InteractionComponent &interaction, ItemComponent &item) {
-		    if (!interaction.isActive) {
-			    return;
-		    }
-		    if (interaction.action == INTERACTION_ACTION::PICK_ITEM) {
-			    if (inputComponent.interact.justPressed) {
-				    return;
-			    }
+		manager, [&](auto& entity, InteractionComponent& interaction, ItemComponent& item) {
+			if (!interaction.isActive) {
+				return;
+			}
+			if (interaction.action == INTERACTION_ACTION::PICK_ITEM) {
+				if (inputComponent.interact.justPressed) {
+					return;
+				}
 
-		    	world->pushMessageToHud("New Item Added");
-			    manager.getComponent<PartOfLayerComponent>(entity).groupId = character_component.inventory.inventoryWorldId;
-		    }
-	    });
+				world->pushMessageToHud("New Item Added");
+				manager.getComponent<PartOfLayerComponent>(entity).groupId = character_component.inventory.inventoryWorldId;
+			}
+		});
 }
