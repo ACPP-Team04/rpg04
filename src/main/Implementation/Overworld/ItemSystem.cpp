a6ae7861f730f6861bf17573fb9f56f85ba75b9a
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-05-22 16:12:34 +0200

    Add picked up item to the gamestate

--- src/main/Implementation/Overworld/ItemSystem.cpp
+++ src/main/Implementation/Overworld/ItemSystem.cpp
@@ -1,10 +1,12 @@
 #pragma once
 #include "Abstract/Overwordl/ItemSystem.hpp"
 
+#include "Abstract/GameState/GameState.hpp"
 #include "Abstract/Overwordl/Components/InputComponent.hpp"
 #include "Abstract/Overwordl/Components/InteractionComponent.hpp"
 #include "Abstract/Overwordl/Components/InventoryComponent.hpp"
 #include "Abstract/Overwordl/Components/ItemComponent.hpp"
+#include "Abstract/Overwordl/Components/PersistanceComponent.hpp"
 #include "Abstract/Overwordl/Components/Player_Component.hpp"
 #include "Abstract/Overwordl/Components/RenderComponent.hpp"
 #include "Abstract/Overwordl/WorldParser.hpp"
@@ -28,6 +30,11 @@ void ItemSystem::update()
 		    if (interaction.action == INTERACTION_ACTION::PICK_ITEM) {
 			    if (inputComponent.interact.justPressed) {
 				    return;
+			    }
+			    if (manager.hasComponent<PersistanceComponent>(entity)) {
+				    std::string uuid = manager.getComponent<PersistanceComponent>(entity).uuid;
+				    GameState::getInstance().deadUniqueEntities.insert(uuid);
+				    spdlog::info("Item picked up! UUID {} added to dead entities.", uuid);
 			    }
 			    inventoryComponent.addItem(entity, item.itemType);
 			    this->manager.removeComponentFromEntity<RenderComponent>(entity);
