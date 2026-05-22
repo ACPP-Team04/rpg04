4572ffd73ede48d1e06567affb735b24e458dda8
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-05-22 22:05:45 +0200

    Delete componts outside of the view

--- src/main/Implementation/Overworld/ItemSystem.cpp
+++ src/main/Implementation/Overworld/ItemSystem.cpp
@@ -9,6 +9,9 @@
 #include "Abstract/Overwordl/Components/RenderComponent.hpp"
 #include "Abstract/Overwordl/WorldParser.hpp"
 #include "Abstract/Utils/WorldUtlis.hpp"
+#include <Abstract/Overwordl/Components/BoundingBoxComponent.hpp>
+#include <Abstract/Overwordl/Components/CollisionComponent.hpp>
+#include <spdlog/spdlog.h>
 
 ItemSystem::ItemSystem(ArchetypeManager &manager) : System(manager) {}
 
@@ -20,18 +23,31 @@ void ItemSystem::update()
 	}
 	InputComponent &inputComponent = inputComponentOptional.value();
 	InventoryComponent &inventoryComponent = WorldUtils::getPlayersComponent<InventoryComponent>(manager).value();
+
+	std::vector<EntityID> itemsToPickup;
 	WorldUtils::viewInCurrentLayer<InteractionComponent, ItemComponent>(
 	    manager, [&](auto &entity, InteractionComponent &interaction, ItemComponent &item) {
 		    if (!interaction.isActive) {
 			    return;
 		    }
 		    if (interaction.action == INTERACTION_ACTION::PICK_ITEM) {
-			    if (inputComponent.interact.justPressed) {
+			    if (!inputComponent.interact.justPressed) {
 				    return;
 			    }
 			    inventoryComponent.addItem(entity, item.itemType);
-			    this->manager.removeComponentFromEntity<RenderComponent>(entity);
-			    this->manager.removeComponentFromEntity<InteractionComponent>(entity);
+			    itemsToPickup.push_back(entity);
+			    spdlog::info("Picked up item: {}", item.name);
 		    }
 	    });
+	for (EntityID entity : itemsToPickup) {
+		manager.removeComponentFromEntity<RenderComponent>(entity);
+		manager.removeComponentFromEntity<InteractionComponent>(entity);
+
+		if (manager.hasComponent<CollisionComponent>(entity)) {
+			manager.removeComponentFromEntity<CollisionComponent>(entity);
+		}
+		if (manager.hasComponent<BoundIngBoxComponent>(entity)) {
+			manager.removeComponentFromEntity<BoundIngBoxComponent>(entity);
+		}
+	}
 }
