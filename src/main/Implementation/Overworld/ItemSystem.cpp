ebd6803224485f54ed603c21ad87d6c433514485
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-05-22 16:12:34 +0200

    Add picked up item to the gamestate

--- src/main/Implementation/Overworld/ItemSystem.cpp
+++ src/main/Implementation/Overworld/ItemSystem.cpp
@@ -6,6 +6,7 @@
 #include "Abstract/Overwordl/Components/InventoryComponent.hpp"
 #include "Abstract/Overwordl/Components/IsInInventoryComponent.hpp"
 #include "Abstract/Overwordl/Components/ItemComponent.hpp"
+#include "Abstract/Overwordl/Components/PersistanceComponent.hpp"
 #include "Abstract/Overwordl/Components/Player_Component.hpp"
 #include "Abstract/Overwordl/Components/RenderComponent.hpp"
 #include "Abstract/Overwordl/WorldParser.hpp"
@@ -17,29 +18,34 @@ ItemSystem::ItemSystem(ArchetypeManager& manager) : System(manager) {}
 
 void ItemSystem::update()
 {
-	WorldComponent *world = WorldUtils::getWorld(manager);
+	WorldComponent* world = WorldUtils::getWorld(manager);
 	EntityID player = WorldUtils::getPlayer(manager).value();
 
 	if (!manager.hasComponent<InputComponent>(player)) {
 		return;
 	}
-	CharacterComponent &character_component = manager.getComponent<CharacterComponent>(player);
+	CharacterComponent& character_component = manager.getComponent<CharacterComponent>(player);
 	std::cout << "CharacterCatch" << std::endl;
-	InputComponent &inputComponent = manager.getComponent<InputComponent>(player);
+	InputComponent& inputComponent = manager.getComponent<InputComponent>(player);
 	std::cout << character_component.inventory.items.size() << std::endl;
 	WorldUtils::viewInCurrentLayer<InteractionComponent, ItemComponent>(
-	    manager, [&](auto &entity, InteractionComponent &interaction, ItemComponent &item) {
-		    if (!interaction.isActive) {
-			    return;
-		    }
-		    if (interaction.action == INTERACTION_ACTION::PICK_ITEM) {
-			    if (inputComponent.interact.justPressed) {
-				    return;
-			    }
+		manager, [&](auto& entity, InteractionComponent& interaction, ItemComponent& item) {
+			if (!interaction.isActive) {
+				return;
+			}
+			if (interaction.action == INTERACTION_ACTION::PICK_ITEM) {
+				if (inputComponent.interact.justPressed) {
+					return;
+				}
 
-			    world->pushMessageToHud("New Item Added");
-			    manager.getComponent<PartOfLayerComponent>(entity).groupId =
-			        character_component.inventory.inventoryWorldId;
-		    }
-	    });
+				world->pushMessageToHud("New Item Added");
+				manager.getComponent<PartOfLayerComponent>(entity).groupId =
+					character_component.inventory.inventoryWorldId;
+				if (manager.hasComponent<PersistanceComponent>(entity)) {
+					std::string uuid = manager.getComponent<PersistanceComponent>(entity).uuid;
+					GameState::getInstance().deadUniqueEntities.insert(uuid);
+					spdlog::info("Item picked up! UUID {} added to dead entities.", uuid);
+				}
+			}
+		});
 }
