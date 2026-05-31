3297b2fa506b173808a0f6c3f9da0b5d580b0cfd
Jungerindy <102881736+Jungerindy@users.noreply.github.com>
2026-05-31 18:46:17 +0200

    Also save interactionStates of dialogs

--- src/main/Abstract/Persistance/SaveManager.hpp
+++ src/main/Abstract/Persistance/SaveManager.hpp
@@ -15,6 +15,9 @@ class SaveManager {
 
 	static void injectPlayer(ArchetypeManager &manager, const nlohmann::json &playerJson, EntityID &playerID);
 	static void injectWorldComponent(ArchetypeManager &manager, const nlohmann::json &worldStateJson);
+	static void injectDoors(ArchetypeManager &manager, const nlohmann::json &doorStates);
+	static void injectDialogs(ArchetypeManager &manager, const nlohmann::json &dialogStates,
+	                          const nlohmann::json &interactionStates);
 
   private:
 	static std::string getSaveFilePath(int slotIndex);
