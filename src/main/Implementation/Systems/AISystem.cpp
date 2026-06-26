78ea5b578ef0957d50305a53ba61942b635eddd0
Jona Müller <102881736+Jungerindy@users.noreply.github.com>
2026-06-26 09:05:11 +0200

    Comment out BattleComponent retrieval in AISystem
    Commented out the retrieval of the BattleComponent for AI.

--- src/main/Implementation/Systems/AISystem.cpp
+++ src/main/Implementation/Systems/AISystem.cpp
@@ -16,7 +16,7 @@ AISystem::AISystem(ArchetypeManager &manager) : System(manager) {};
 
 std::optional<EntityID> AISystem::selectTarget(EntityID aiId, const std::vector<EntityID> &participants)
 {
-	auto &aiBattleComp = manager.getComponent<BattleComponent>(aiId);
+	//auto &aiBattleComp = manager.getComponent<BattleComponent>(aiId);
 
 	std::vector<EntityID> validTargets = this->getValidTargets(aiId, participants);
 
