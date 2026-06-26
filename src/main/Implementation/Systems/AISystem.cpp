2b0eb556d5968e9dd58a7fc5220305986f94a0d1
Jona Müller <102881736+Jungerindy@users.noreply.github.com>
2026-06-26 09:22:09 +0200

    Remove commented code and add test variable

--- src/main/Implementation/Systems/AISystem.cpp
+++ src/main/Implementation/Systems/AISystem.cpp
@@ -16,8 +16,7 @@ AISystem::AISystem(ArchetypeManager &manager) : System(manager) {};
 
 std::optional<EntityID> AISystem::selectTarget(EntityID aiId, const std::vector<EntityID> &participants)
 {
-	//auto &aiBattleComp = manager.getComponent<BattleComponent>(aiId);
-
+	auto test = 42;
 	std::vector<EntityID> validTargets = this->getValidTargets(aiId, participants);
 
 	if (validTargets.empty()) {
